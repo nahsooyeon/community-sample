bdab5e2a6c90a0504ce5e3aa1a8e8a0957eb4fcb
nahsooyeon <74342611+nahsooyeon@users.noreply.github.com>
2023-09-11 00:00:49 +0900

    🔧 수정: 디렉토리 경로 수정 및 eslint 규칙 추가 및 적용 (#8)
    * 🔧 수정: 디렉토리 경로 수정 및 eslint 규칙 추가 및 적용
    * 🔧 추가: editorconfig
    * 🚨 수정: 프리티어 적용

--- src/lib/reactQuery.ts
+++ src/lib/reactQuery.ts
@@ -1,6 +1,4 @@
-import type { AxiosError } from 'axios';
-import type { UseQueryOptions, UseMutationOptions } from 'react-query';
-import { QueryClient, QueryCache } from 'react-query';
+import { QueryClient, QueryCache } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,45 +10,14 @@ const queryClient = new QueryClient({
     },
   },
   queryCache: new QueryCache({
-    onError: (error) => {
-      checkRefreshIssued(error as OnErrorType<null>);
+    onError: () => {
+      checkRefreshIssued();
     },
   }),
 });
 
-const checkRefreshIssued = async (error: OnErrorType<null>) => {
+const checkRefreshIssued = async () => {
   /* JWT 갱신 에러 처리 */
 };
 
-export type OnErrorType<T> = {
-  response: {
-    data: {
-      [key: string]: T;
-    };
-    status: number;
-    errors: ErrorType[];
-    headers: {
-      map: {
-        [key: string]: string;
-      };
-    };
-  };
-};
-
 export default queryClient;
-
-type ErrorType = {
-  message: string;
-};
-export type MutationConfig<MutationFnType extends (...args: any) => any> = UseMutationOptions<
-  ExtractFnReturnType<MutationFnType>,
-  AxiosError,
-  Parameters<MutationFnType>[0]
->;
-
-export type ExtractFnReturnType<FnType extends (...args: any) => any> = ReturnType<FnType>;
-
-export type QueryConfig<QueryFnType extends (...args: any) => any> = Omit<
-  UseQueryOptions<ExtractFnReturnType<QueryFnType>>,
-  'queryKey' | 'queryFn'
->;
---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+import { QueryClientProvider, Hydrate } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "@/styles/globals.css";
+import queryClient from "@/lib/reactQuery";
+
+const MyApp = (props: AppProps) => (
+  <QueryClientProvider client={queryClient}>
+    <Hydrate state={props.pageProps.dehydratedState}>
+      <props.Component {...props.pageProps} />
+    </Hydrate>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
+export default MyApp;
