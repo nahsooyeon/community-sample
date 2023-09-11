301a85380345ca33abb99e27c42a0e699938cdf7
nahsooyeon <74342611+nahsooyeon@users.noreply.github.com>
2023-09-11 18:32:48 +0900

    🔨 추가: 로그인 페이지 (#11)
    GOOD

---
+++ src/pages/Login/index.page.tsx
@@ -0,0 +1,12 @@
+const LoginPage = () => (
+  <div>
+    로그인 페이지
+    <form>
+      <input type="text" placeholder="아이디" />
+      <input type="password" placeholder="비밀번호" />
+      <button type="submit">로그인</button>
+    </form>
+  </div>
+);
+
+export default LoginPage;
